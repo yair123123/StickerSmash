85fc0703f53e0dc0e1cf364cd89ed7dccb10a664
DELL <ytykhn8@gmail.com>
2025-01-01 14:04:13 +0200

    first commit

---
+++ app/about.tsx
@@ -0,0 +1,30 @@
+import { Link } from 'expo-router';
+import { Text, View, StyleSheet } from 'react-native';
+
+export default function AboutScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>About screen</Text>
+    <Link href="/Home" style={styles.button}>
+        Go to Home screen
+    </Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: '#fff',
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
+    color: '#fff',
+  }
+});
